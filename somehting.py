f5bac342f7cdc9b2f401aacafcfbb8d4554502fc
IamAyman <theycallmethedr@gmail.com>
2014-03-01 21:59:29 +0100

    added a comment before merging and after the pull request

--- somehting.py
+++ somehting.py
@@ -4,3 +4,5 @@ import re, urllib2, json
 
 print "Dont mind me :3  Jsut trying somehting :P"
 print re.findall('[0,1]+',json.load(urllib2.urlopen('https ://api.duckduckgo. com/?q=I\'m+just+trying+something+to+binary&format=json'.replace(" ","")))["Answer"])[0]
+
+#Added this comment before merging and after the pull request
